d2074a9a1774c12ae79fc930bc16914b1e81ccf1
Sharon Kerketta <sam8298093735@gmail.com>
2019-09-19 03:49:08 +0530

    reverting back to default viewset

--- operations/books/views.py
+++ operations/books/views.py
@@ -1,29 +1,29 @@
 from rest_framework import viewsets
 from operations.books.serializers import BookSerializer,AuthorOnlySerializer, CategoryOnlySerializer
 from operations.models import Book,Author,Category
-from rest_framework.response import Response
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
+# def get_queryset(self):
+#         queryset= Author.objects.all()
+#         print(queryset)
+#         data = list(Author.objects.all())
+#         authors={}
+#         list_of_authors=[]
+#         for author in data :
+#             list_of_authors.append(author.name)
+#         authors['Author']=list_of_authors
+#         print(authors)
+#         return Author.objects.values("author",list_of_authors)
+        
+
 class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorOnlySerializer
-    
-    def list(self,request):
-        authors = list(Author.objects.all())
-        list_of_authors=[]
-        for author in authors :
-            list_of_authors.append(author.name)
-        return Response({'authors':list_of_authors})
-        
+    queryset = Author.objects.all()
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategoryOnlySerializer
-    def list(self,request):
-        categories = list(Category.objects.all())
-        list_of_categories=[]
-        for category in categories :
-            list_of_categories.append(category.name)
-        return Response({'categories':list_of_categories})
+    queryset = Category.objects.all()
 
