83ba9f09c47fd5ed0387a67982053b509a2fd9a9
Sharon Kerketta <sam8298093735@gmail.com>
2019-09-19 12:44:51 +0530

    Correcting the deletion and retrieval and listing as list of author-names

--- operations/books/views.py
+++ operations/books/views.py
@@ -1,29 +1,32 @@
 from rest_framework import viewsets
 from operations.books.serializers import BookSerializer,AuthorOnlySerializer, CategoryOnlySerializer
 from operations.models import Book,Author,Category
+from rest_framework.response import Response
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
-# def get_queryset(self):
-#         queryset= Author.objects.all()
-#         print(queryset)
-#         data = list(Author.objects.all())
-#         authors={}
-#         list_of_authors=[]
-#         for author in data :
-#             list_of_authors.append(author.name)
-#         authors['Author']=list_of_authors
-#         print(authors)
-#         return Author.objects.values("author",list_of_authors)
-        
-
 class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorOnlySerializer
     queryset = Author.objects.all()
+    def list(self,request):
+        data = list(Author.objects.all())
+        authors={}
+        list_of_authors=[]
+        for author in data :
+            list_of_authors.append(author.name)
+        return Response({"authors" : list_of_authors})
+        
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategoryOnlySerializer
     queryset = Category.objects.all()
+    def list(self,request):
+        data = list(Category.objects.all())
+        categories={}
+        list_of_categories=[]
+        for category in data :
+            list_of_categories.append(category.name)
+        return Response({"categories" : list_of_categories})
 
