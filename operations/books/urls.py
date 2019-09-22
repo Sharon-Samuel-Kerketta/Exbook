60b8328f08a03db876ebfe0a3468097771bbd40f
Sharon Kerketta <sam8298093735@gmail.com>
2019-09-23 00:23:21 +0530

    Added the static file's url to the urlpatterns in urls.py

--- operations/books/urls.py
+++ operations/books/urls.py
@@ -2,6 +2,8 @@ from operations.books.serializers import BookSerializer,AuthorSerializer,Categor
 from rest_framework.routers import DefaultRouter
 from operations.books.views import BookViewSet,AuthorViewSet,CategoryViewSet,BookSearchViewSet
 from django.urls import reverse
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
@@ -9,3 +11,5 @@ router.register(r'authors',AuthorViewSet,base_name='authors')
 router.register(r'categories',CategoryViewSet,base_name='categories')
 router.register(r'search',BookSearchViewSet, base_name='search' )
 urlpatterns = router.urls
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
